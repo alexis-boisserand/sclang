4b717d8c87fbdf235f5d6d3a07e60e4b20e53a64
Alexis Boisserand <alexis.boisserand@gmail.com>
2020-02-15 09:32:39 -0500

    Renames to scgen

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-![Python application](https://github.com/alexis-boisserand/fsmgen/workflows/Python%20application/badge.svg)
-# fsmgen
+![Python application](https://github.com/alexis-boisserand/scgen/workflows/Python%20application/badge.svg)
+# scgen
 State machine generation and visualization tools
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-import fsmgen
+import scgen
 import schematics
 
 current_dir = os.path.dirname(__file__)
@@ -12,57 +12,57 @@ def local(file_name):
 
 def test_simplest():
     with open(local('simplest.yaml'), 'r') as f:
-        simplest = fsmgen.load(f)
+        simplest = scgen.load(f)
     assert len(simplest.states) == 2
     assert simplest.states[0].name == "Occupied"
     assert simplest.states[0].transitions[0].event == "PIR_HIT"
 
 
 def test_none():
-    with pytest.raises(fsmgen.LoadingError):
-        fsmgen.load(None)
+    with pytest.raises(scgen.LoadingError):
+        scgen.load(None)
 
 
 def test_garbage():
-    with pytest.raises(fsmgen.LoadingError):
-        fsmgen.load("garbage")
+    with pytest.raises(scgen.LoadingError):
+        scgen.load("garbage")
 
 
 def test_state_no_name():
     with open(local('no_name.yaml'), 'r') as f:
-        with pytest.raises(fsmgen.LoadingError) as exc:
-            fsmgen.load(f)
+        with pytest.raises(scgen.LoadingError) as exc:
+            scgen.load(f)
     cause = exc.value.__cause__
     assert type(cause) is schematics.exceptions.DataError
 
 
 def test_state_names_not_unique():
     with open(local('names_not_unique.yaml'), 'r') as f:
-        with pytest.raises(fsmgen.LoadingError) as exc:
-            fsmgen.load(f)
+        with pytest.raises(scgen.LoadingError) as exc:
+            scgen.load(f)
     cause = exc.value.__cause__
     assert 'unique' in str(cause)
 
 
 def test_invalid_transition_target_name():
     with open(local('invalid_transition_target_name.yaml'), 'r') as f:
-        with pytest.raises(fsmgen.LoadingError) as exc:
-            fsmgen.load(f)
+        with pytest.raises(scgen.LoadingError) as exc:
+            scgen.load(f)
     cause = exc.value.__cause__
     assert 'invalid transition target name' in str(cause)
 
 
 def test_invalid_transition_event():
     with open(local('invalid_transition_event.yaml'), 'r') as f:
-        with pytest.raises(fsmgen.LoadingError) as exc:
-            fsmgen.load(f)
+        with pytest.raises(scgen.LoadingError) as exc:
+            scgen.load(f)
     cause = exc.value.__cause__
     assert 'transition events must be unique' in str(cause)
 
 
 def test_unreachable_state():
     with open(local('unreachable_state.yaml'), 'r') as f:
-        with pytest.raises(fsmgen.LoadingError) as exc:
-            fsmgen.load(f)
+        with pytest.raises(scgen.LoadingError) as exc:
+            scgen.load(f)
     cause = exc.value.__cause__
     assert 'unreachable' in str(cause)
