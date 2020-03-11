0fb0e78b746d758540a700eb8cafd47a59eba282
Alexis Boisserand <alexis.boisserand@gmail.com>
2020-03-11 00:20:15 -0400

    Moves the code tool into its own folder.

--- sclang/code.py
+++ sclang/code.py
@@ -1,23 +1,28 @@
 import os
-import sys
-import argparse
 from jinja2 import Environment, FileSystemLoader
-from .parser import parse, ParsingError
 from .normalize import upper_case, lower_case, camel_case, lower_camel_case
 
 current_dir = os.path.dirname(__file__)
 template_dir = os.path.join(current_dir, 'templates')
 
 
+def type_naming_style(*args):
+    return lower_case(*args) + '_t'
+
+
+style = {
+    'function': lower_case,
+    'variable': lower_case,
+    'type': type_naming_style,
+    'constant': upper_case
+}
+
+
 def code(name, state_chart, output_dir):
-    filters = {
-        func.__name__: func
-        for func in [upper_case, lower_case, camel_case, lower_camel_case]
-    }
     env = Environment(loader=FileSystemLoader(template_dir),
                       trim_blocks=True,
                       lstrip_blocks=True)
-    env.filters.update(filters)
+    env.filters.update(style)
     inputs = [('state_chart_header.jinja', 'h'),
               ('state_chart_impl.jinja', 'c')]
     file_prefix = lower_case(name)
@@ -26,32 +31,5 @@ def code(name, state_chart, output_dir):
         output = os.path.join(output_dir, '.'.join([file_prefix, ext]))
         template.stream(state_chart_name=name,
                         state_chart=state_chart,
+                        style=style,
                         file_prefix=file_prefix).dump(output)
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='Generates the statechart corresponding C code.')
-    parser.add_argument('-o',
-                        '--output',
-                        default='.',
-                        help='code output directory')
-    parser.add_argument('-sc',
-                        '--state_chart',
-                        required=True,
-                        type=argparse.FileType('r'),
-                        help='statechart declaration file')
-    args = parser.parse_args()
-
-    try:
-        with args.state_chart:
-            state_chart = parse(args.state_chart.read())
-    except ParsingError as exc:
-        print('Failed to read {}: {}'.format(args.state_chart.name, str(exc)))
-        sys.exit(1)
-
-    code(os.path.splitext(args.state_chart.name)[0], state_chart, args.output)
-
-
-if __name__ == '__main__':
-    main()
---
+++ sclang/tools/code.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import argparse
+from ..parser import parse, ParsingError
+from ..code import code
+
+current_dir = os.path.dirname(__file__)
+template_dir = os.path.join(current_dir, 'templates')
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Generates the statechart corresponding C code.')
+    parser.add_argument('state_chart',
+                        type=argparse.FileType('r'),
+                        help='statechart declaration file')
+    parser.add_argument('-o',
+                        '--output',
+                        default='.',
+                        help='code output directory')
+
+    args = parser.parse_args()
+
+    try:
+        with args.state_chart:
+            state_chart = parse(args.state_chart.read())
+    except ParsingError as exc:
+        print('Failed to read {}: {}'.format(args.state_chart.name, str(exc)))
+        sys.exit(1)
+
+    code(os.path.splitext(args.state_chart.name)[0], state_chart, args.output)
+
+
+if __name__ == '__main__':
+    main()
